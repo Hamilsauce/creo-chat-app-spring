bfa81633e1b3ba963857100788de644165869b15
Jake Hamilton <jacob@theleaguestock.com>
2022-05-24 07:24:52 -0500

    init spring commit; working proj

---
+++ src/test/java/com/springboot/demo2/springbootdemo2/SpringBootDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.demo2.springbootdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
